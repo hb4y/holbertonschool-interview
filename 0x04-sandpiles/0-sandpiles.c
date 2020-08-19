390e8aea464ed2dcb4ffe56ce1139e50761c2a79
hb4y <hugobaymon@gmail.com>
2020-08-19 04:38:19 +0000

    Initial commit

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: 3x3 array of integers
+ * @grid2: 3x3 array of integers
+ * Return: void
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,6 @@
+#ifndef SAND_H
+#define SAND_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* BIN_H */
