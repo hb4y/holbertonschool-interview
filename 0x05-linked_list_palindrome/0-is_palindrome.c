e71aeffcc6a8f990e14f32c6034beca411184919
hb4y <hugobaymon@gmail.com>
2020-08-24 21:36:30 +0000

    Reverse singly linked list

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * reverse - reverse a listint_t list
+ * @head: pointer to list
+ * Return: pointer to the new head
+ */
+listint_t *reverse(listint_t *head)
+{
+	listint_t *prev, *next;
+
+	prev = NULL;
+	next = NULL;
+
+	while (head)
+	{
+		next = head->next;
+		head->next = prev;
+		prev = head;
+		head = next;
+	}
+	return (prev);
+}
+
+/**
+ * is_palindrome - frees a listint_t list
+ * @head: pointer to list
+ * Return: 0 is not palindrome 1 if palimdrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast;
+
+	slow = *head;
+	fast = *head;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	slow = reverse(slow);
+	fast = *head;
+
+	while (!slow)
+	{
+		if (slow->n != fast->n)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (1);
+}
