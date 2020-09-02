e02069264b3c505696c3a50752b39b9064babc1c
hb4y <hugobaymon@gmail.com>
2020-09-02 22:03:15 +0000

    when there's no more logs

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,24 +11,30 @@ import sys
 
 
 def _print(file_size, status):
-    print("File size: {}".format(file_size[0]))
-    for key in sorted(status.keys()):
-        if status[key] != 0:
-            print("{}: {}".format(key, status[key]))
+    try:
+        print("File size: {}".format(file_size[0]))
+        for key in sorted(status.keys()):
+            if status[key] != 0:
+                print("{}: {}".format(key, status[key]))
+    except:
+        pass
 
 
 def parser(log, counter, file_size, status):
-    if log[8].isdigit():
-        file_size[0] += int(log[8])
+    try:
+        if log[8].isdigit():
+            file_size[0] += int(log[8])
 
-    if log[7].isdigit():
-        status[int(log[7])] += 1
+        if log[7].isdigit():
+            status[int(log[7])] += 1
 
-    if counter[0] == 9:
-        _print(file_size, status)
-        counter[0] = 0
-    else:
-        counter[0] += 1
+        if counter[0] == 9:
+            _print(file_size, status)
+            counter[0] = 0
+        else:
+            counter[0] += 1
+    except:
+        pass
 
 if __name__ == "__main__":
     counter = [0]
@@ -38,7 +44,11 @@ if __name__ == "__main__":
     while True:
         try:
             log = input().split()
-            parser(log, counter, file_size, status)
+            if log:
+                parser(log, counter, file_size, status)
+            else:
+                _print(file_size, status)
+                exit()
         except KeyboardInterrupt:
             _print(file_size, status)
             exit()
