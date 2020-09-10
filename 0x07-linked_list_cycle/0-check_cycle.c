b19b3b5e52a49a5af7fcdbd4b45c3a4097be66f3
hb4y <hugobaymon@gmail.com>
2020-09-10 01:30:49 +0000

    check if a list has a cicle

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - check if a list have a cicle
+ * @list: pointer to list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (!list || !(list->next))
+		return (0);
+
+	fast = list;
+	slow = list;
+
+	while (slow->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast || fast->next == slow)
+			return (1);
+	}
+	return (0);
+}
