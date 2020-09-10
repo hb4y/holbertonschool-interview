d257cab9ecc5d28cc9eb07d53c7b00b85c6d936d
hb4y <hugobaymon@gmail.com>
2020-09-10 01:39:52 +0000

    delete a compar

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -21,7 +21,7 @@ int check_cycle(listint_t *list)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-		if (slow == fast || fast->next == slow)
+		if (slow == fast)
 			return (1);
 	}
 	return (0);
