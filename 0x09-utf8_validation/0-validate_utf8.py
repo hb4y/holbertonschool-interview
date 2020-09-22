78a3598719dac3c21c79ffd4df4251c29ff03848
hb4y <hugobaymon@gmail.com>
2020-09-22 02:17:49 +0000

    simpler solution taking advantage of python

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,19 +5,11 @@ Determines if a given data set represents a valid UTF-8 encoding.
 
 
 def validUTF8(data):
-    n_byte = 0
-    for number in data:
-        mask = 1 << 0b111
-        if n_byte == 0:
-            while mask & number:
-                n_byte += 1
-                mask = mask >> 1
-            if n_byte == 0:
-                continue
-            if n_byte == 1 or n_byte > 4:
-                return False
-        else:
-            if n_byte >> 6 != 0b10:
-                return False
-        n_byte -= 1
-    return n_byte == 0
+    """
+    Pythonistic solution
+    """
+    try:
+        bytes(number & 0xFF for number in data).decode()
+        return True
+    except UnicodeDecodeError:
+        return False
