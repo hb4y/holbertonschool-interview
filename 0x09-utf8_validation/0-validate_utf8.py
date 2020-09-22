32574c6221d92246715988cc1d22f335af6d7f6a
hb4y <hugobaymon@gmail.com>
2020-09-22 01:54:29 +0000

    check valid uft-8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Determines if a given data set represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+	n_byte = 0
+	for number in data:
+		mask = 1 << 0b111
+		if n_byte == 0:
+			while mask & number:
+				n_byte += 1
+				mask = mask >> 1
+			if n_byte == 0:
+				continue
+			elif n_byte == 1 or n_byte > 4:
+				return False
+		else:
+			if n_byte >> 6 != 0b10:
+				return False
+		n_byte -= 1
+	return n_byte == 0 
