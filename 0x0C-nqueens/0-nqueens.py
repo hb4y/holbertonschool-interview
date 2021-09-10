6d92939c704428ee22352f5b76db09617fd483ef
hb4y <hugobaymon@gmail.com>
2021-09-09 23:49:40 -0500

    n queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 """
-The N queens puzzle:
-
-Challenge of placing N non-attacking queens on an N×N chessboard.
-
-Solves the N queens problem.
+The N queens puzzle is the challenge of placing
+N non-attacking queens on an N×N chessboard.
 """
 
 import sys
@@ -16,60 +13,65 @@ if __name__ == '__main__':
         sys.exit(1)
 
     try:
-        n_queen = int(sys.argv[1])
+        n_qns = int(sys.argv[1])
     except BaseException:
         print('N must be a number')
         sys.exit(1)
 
-    if n_queen < 4:
+    if n_qns < 4:
         print('N must be at least 4')
         sys.exit(1)
 
-    chest_board = [[0 for col in range(n_queen)] for row in range(n_queen)]
+    chess_board = [[0 for col in range(n_qns)] for row in range(n_qns)]
 
     def main():
-        b = [[0 for j in range(n_queen)] for i in range(n_queen)]
-        recursive_func(b, 0)
+        aux_b = [[0 for j in range(n_qns)] for i in range(n_qns)]
+        recursive_func(aux_b, 0)
         return
 
-    def recursive_func(b, c):
-        if (c == n_queen):
-            solution(b)
+    def recursive_func(aux_b, aux_c):
+        if (aux_c == n_qns):
+            solution(aux_b)
+
             return True
         ret = False
-        for i in range(n_queen):
-            if (validate_pos(b, i, c)):
-                b[i][c] = 1
-                ret = recursive_func(b, c + 1) or ret
-                b[i][c] = 0
+        for i in range(n_qns):
+            if (validate_pos(aux_b, i, aux_c)):
+                aux_b[i][aux_c] = 1
+                ret = recursive_func(aux_b, aux_c + 1) or ret
+                aux_b[i][aux_c] = 0
+
         return ret
 
-    def validate_pos(b, r, c):
-        for i in range(c):
-            if (b[r][i]):
+    def validate_pos(aux_b, r, aux_c):
+        for i in range(aux_c):
+            if (aux_b[r][i]):
                 return False
+
         i = r
-        j = c
+        j = aux_c
         while i >= 0 and j >= 0:
-            if(b[i][j]):
+            if(aux_b[i][j]):
                 return False
+
             i = i - 1
             j = j - 1
         i = r
-        j = c
-        while j >= 0 and i < n_queen:
-            if(b[i][j]):
+        j = aux_c
+        while j >= 0 and i < n_qns:
+            if(aux_b[i][j]):
                 return False
             i = i + 1
             j = j - 1
         return True
 
-    def solution(b):
+    def solution(aux_b):
         solve = []
-        for i in range(n_queen):
-            for j in range(n_queen):
-                if(b[i][j] == 1):
+        for i in range(n_qns):
+            for j in range(n_qns):
+                if(aux_b[i][j] == 1):
                     solve.append([i, j])
         print(solve)
         solve.clear()
+
     main()
