873cebcf86e5c1b779ad595411827e010d18506d
hb4y <hugobaymon@gmail.com>
2021-09-10 00:34:15 -0500

    semistandart fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -9,7 +9,7 @@ const URL = 'https://swapi-api.hbtn.io/api/';
 const r = require('request');
 const mov = process.argv[2];
 
-async function sw_get (url) {
+async function swGet (url) {
   return new Promise(function (resolve, reject) {
     r.get(url, function (err, resp, body) {
       if (err) {
@@ -21,11 +21,11 @@ async function sw_get (url) {
   });
 }
 (async () => {
-  return sw_get(URL + 'films/' + mov);
-})().then(async (curr_movie) => {
-  if (curr_movie.detail !== 'Not found') {
-    for (const ch of curr_movie.characters) {
-      const character = await sw_get(ch);
+  return swGet(URL + 'films/' + mov);
+})().then(async (currMovie) => {
+  if (currMovie.detail !== 'Not found') {
+    for (const ch of currMovie.characters) {
+      const character = await swGet(ch);
       if (character.detail === undefined) {
         console.log(character.name);
       }
