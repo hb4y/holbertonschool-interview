cb712ba1ba1c43e95a87f095953d229ae3d78187
hb4y <hugobaymon@gmail.com>
2021-09-28 14:45:12 -0500

    [header] .h

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef BIN_ADV
+#define BIN_ADV
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(int *array, int start, int end);
+int b_search(int *array, int first, int last, int value);
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
